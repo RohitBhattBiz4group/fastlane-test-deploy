1188b13a7a13dabaae7d9e0ca89185d755328767
Rohit Bhatt <rohit.bhatt@biz4group.com>
2026-03-25 15:53:50 +0530

    code

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    padding: 24,
+    padding: 25,
     backgroundColor: "#ffffff",
   },
   title: {
